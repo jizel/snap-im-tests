e95b429a0e69c66f7566b5186e3d91f9917b6c49
Jiri Sedlacek <jiri@snapshot.travel>
2015-12-09 09:38:16 +0100

    cleaning of database before all identity module tests - fix

--- dp-api-tests/src/test/java/travel/snapshot/dp/qa/serenity/DbUtilsSteps.java
+++ dp-api-tests/src/test/java/travel/snapshot/dp/qa/serenity/DbUtilsSteps.java
@@ -14,7 +14,9 @@ public class DbUtilsSteps {
     static final String DELETE_USER_PROPERTYSET = "delete  from User_PropertySet";
     static final String DELETE_PROPERTY_PROPERTYSET = "delete  from Property_PropertySet";
     static final String DELETE_PROPERTY = "delete  from Property";
+    static final String UPDATE_PROPERTY_SET = "update Property_Set set parent_id = null where parent_id is not null";
     static final String DELETE_PROPERTY_SET = "delete  from Property_Set";
+    static final String UPDATE_CUSTOMER = "update Customer set parent_id = null where parent_id is not null";
     static final String DELETE_CUSTOMER = "delete  from Customer";
     static final String DELETE_USER = "delete  from User";
     static final String DELETE_ADDRESS = "delete  from Address";
@@ -32,7 +34,9 @@ public class DbUtilsSteps {
         dbHelper.identityDb().update(DELETE_USER_PROPERTYSET);
         dbHelper.identityDb().update(DELETE_PROPERTY_PROPERTYSET);
         dbHelper.identityDb().update(DELETE_PROPERTY);
+        dbHelper.identityDb().update(UPDATE_PROPERTY_SET);
         dbHelper.identityDb().update(DELETE_PROPERTY_SET);
+        dbHelper.identityDb().update(UPDATE_CUSTOMER);
         dbHelper.identityDb().update(DELETE_CUSTOMER);
         dbHelper.identityDb().update(DELETE_USER);
         dbHelper.identityDb().update(DELETE_ADDRESS);
