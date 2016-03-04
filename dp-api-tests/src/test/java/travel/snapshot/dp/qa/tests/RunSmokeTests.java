ea68ace04513a53c6096f131e80016f97012e9f6
Tomas Vlcek <tomas.vlcek@snapshot.travel>
2016-03-04 14:04:31 +0100

    Serenity modules upgrades, gradle version upgraded

---
+++ dp-api-tests/src/test/java/travel/snapshot/dp/qa/tests/RunSmokeTests.java
@@ -0,0 +1,14 @@
+package travel.snapshot.dp.qa.tests;
+
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features", glue = "travel.snapshot.dp.qa", tags = {"~@skipped","@Smoke"})
+public class RunSmokeTests {
+
+}
