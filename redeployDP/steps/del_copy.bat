4956ac83f7e73b09f052cd1d19dc779e89c11dec
Jiri Zelezny <zelezny@corp.snapshot.travel>
2017-03-30 10:13:05 +0200

    Deploy script changed to contain new WF module location

--- redeployDP/steps/del_copy.bat
+++ redeployDP/steps/del_copy.bat
@@ -1,5 +1,6 @@
 SET WEBAPPS_DIR=%1
 SET DP_DIR=%2
+SET DP_IDENTITY_DIR=%3
 
 echo Deleting webapps..
 pushd %WEBAPPS_DIR%
@@ -7,7 +8,7 @@ call del * /s /q
 call rmdir . /s /q
 
 echo Copying war files to webapps - omiting OTAIntegration!
-copy %DP_DIR%\IdentityModule\build\libs\IdentityModule-1.0.war .
+copy %DP_IDENTITY_DIR%\IdentityModule\build\libs\IdentityModule-1.0.war .
 copy %DP_DIR%\Integration\Twitter\ServiceApi\build\libs\TwitterAnalyticsApi-1.0-SNAPSHOT.war .
 copy %DP_DIR%\Integration\Instagram\ServiceApi\build\libs\InstagramAnalyticsApi-1.0-SNAPSHOT.war .
 copy %DP_DIR%\Integration\GoogleAnalytics\ServiceApi\build\libs\GoogleAnalyticsApi-1.0-SNAPSHOT.war .
@@ -16,4 +17,4 @@ copy %DP_DIR%\Integration\SocialMediaApi\build\libs\SocialMediaAnalyticsApi-1.0-
 copy %DP_DIR%\Review\build\libs\Review-1.0.war
 copy %DP_DIR%\RateShopper\build\libs\RateShopper-1.0.war .
 copy %DP_DIR%\ConfigurationModule\build\libs\ConfigurationModule-1.0.war .
-copy %DP_DIR%\WebPerformance\build\libs\WebPerformance-1.0.war .
+copy %DP_DIR%\Integration\GoogleAnalytics\ServiceApi\build\libs\WebPerformance-1.0.war .
--- redeployDP/steps/drop_migrateDB.bat
+++ redeployDP/steps/drop_migrateDB.bat
@@ -1,4 +1,5 @@
 SET DP_DIRECTORY=%1
+SET DP_IDENTITY_DIRECTORY=%2
 
 echo Droping all DP tables...
 pushd steps
@@ -9,5 +10,5 @@ pushd %DP_DIRECTORY%\DB\ota_tti\
 call gradle flywayMigrate -Pflyway.url=jdbc:mariadb://localhost:3306 -Pflyway.user=root
 pushd %DP_DIRECTORY%\DB\dp\
 call gradle flywayMigrate -Pflyway.url=jdbc:mariadb://localhost:3306 -Pflyway.user=root
-pushd %DP_DIRECTORY%\DB\identity\
+pushd %DP_IDENTITY_DIRECTORY%\DB\identity\
 call gradle flywayMigrate -Pflyway.url=jdbc:mariadb://localhost:3306 -Pflyway.user=root
