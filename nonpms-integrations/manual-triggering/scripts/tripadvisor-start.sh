d576ca2124b3693a4a79f3385ed959a689bf08d5
Adam Hasik <adam.hasik@snapshot.travel>
2017-07-14 16:43:14 +0200

    DP-2222: Added starter scripts

---
+++ nonpms-integrations/manual-triggering/scripts/tripadvisor-start.sh
@@ -0,0 +1,1 @@
+activemq producer --destination queue://integration.tripadvisor.start --messageCount 1 --message '{\"fireTime\":\"2017-04-21T01:00:00Z\"}'
---
+++ nonpms-integrations/manual-triggering/scripts/twitter-start.sh
@@ -0,0 +1,1 @@
+activemq producer --destination queue://integration.twitter.start --messageCount 1 --message '{\"fireTime\":\"2017-09-25T01:00:00Z\"}'
