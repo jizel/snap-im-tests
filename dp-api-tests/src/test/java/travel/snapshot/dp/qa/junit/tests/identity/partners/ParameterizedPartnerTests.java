e580e1b4ec62c16d50936c8dff5c6d935afa1d88
Jiri Zelezny <zelezny@corp.snapshot.travel>
2017-11-07 11:11:57 +0100

    Gradle wrapper and JUnit5 dependencies update

--- dp-api-tests/src/test/java/travel/snapshot/dp/qa/junit/tests/identity/partners/ParameterizedPartnerTests.java
+++ dp-api-tests/src/test/java/travel/snapshot/dp/qa/junit/tests/identity/partners/ParameterizedPartnerTests.java
@@ -1,13 +1,5 @@
 package travel.snapshot.dp.qa.junit.tests.identity.partners;
 
-import junitparams.FileParameters;
-import junitparams.JUnitParamsRunner;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import travel.snapshot.dp.qa.junit.tests.common.CommonTest;
-
-import java.io.IOException;
-
 import static org.apache.http.HttpStatus.SC_UNPROCESSABLE_ENTITY;
 import static org.hamcrest.core.Is.is;
 import static travel.snapshot.dp.api.identity.resources.IdentityDefaults.PARTNERS_PATH;
@@ -15,12 +7,18 @@ import static travel.snapshot.dp.qa.junit.helpers.CommonHelpers.RESPONSE_CODE;
 import static travel.snapshot.dp.qa.junit.helpers.CommonHelpers.parseResourceFile;
 import static travel.snapshot.dp.qa.junit.helpers.CommonHelpers.sendPostWithBody;
 
-@RunWith(JUnitParamsRunner.class)
-public class ParameterizedPartnerTests extends CommonTest{
-    private static final String EXAMPLES = "src/test/resources/csv/partners/";
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import travel.snapshot.dp.qa.junit.tests.common.CommonTest;
 
-    @Test
-    @FileParameters(EXAMPLES + "checkingErrorCodesForCreatingPartners.csv")
+import java.io.IOException;
+
+public class ParameterizedPartnerTests extends CommonTest{
+
+    private static final String EXAMPLES = "/csv/partners/";
+
+    @ParameterizedTest
+    @CsvFileSource(resources = EXAMPLES + "checkingErrorCodesForCreatingPartners.csv")
     public void checkingErrorCodesForCreatingPartners(String filepath) throws IOException {
         sendPostWithBody(PARTNERS_PATH, parseResourceFile(System.getProperty("user.dir") + filepath))
                 .then()
