acbc94944f636b55b928012130d3f008c763d888
Adam Hasik <adam.hasik@snapshot.travel>
2017-11-30 17:41:15 +0100

    Fixed active profile in DP ETL Tests

--- nonpms-integrations/dp-nonpms-etl-tests/src/test/java/travel/snapshot/dp/qa/nonpms/etl/test/AbstractEtlTest.java
+++ nonpms-integrations/dp-nonpms-etl-tests/src/test/java/travel/snapshot/dp/qa/nonpms/etl/test/AbstractEtlTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfigura
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import travel.snapshot.dp.qa.nonpms.etl.config.TestConfig;
 import travel.snapshot.dp.qa.nonpms.etl.messages.DateRange;
@@ -44,7 +43,6 @@ import javax.jms.Message;
 @SpringBootTest(classes = { Jms.class, TestConfig.class })
 @ImportAutoConfiguration({ JmsAutoConfiguration.class, ActiveMQAutoConfiguration.class })
 @DirtiesContext(classMode =  DirtiesContext.ClassMode.AFTER_CLASS)
-@ActiveProfiles("test")
 public abstract class AbstractEtlTest {
 
     protected abstract Provider getProvider();
