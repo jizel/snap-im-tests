c28a11f1af3f0acdbd2e5b03abfe928186a0ebdf
Jiri Zelezny <zelezny@corp.snapshot.travel>
2017-12-01 15:05:26 +0100

    fixup! Basic user tests rewritten to JUnit

--- dp-api-tests/src/test/java/travel/snapshot/dp/qa/junit/tests/identity/all_endpoints/AllEndpointsParameterizedTests.java
+++ dp-api-tests/src/test/java/travel/snapshot/dp/qa/junit/tests/identity/all_endpoints/AllEndpointsParameterizedTests.java
@@ -1,6 +1,5 @@
 package travel.snapshot.dp.qa.junit.tests.identity.all_endpoints;
 
-import static java.util.Arrays.stream;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_OK;
 import static travel.snapshot.dp.qa.cucumber.serenity.BasicSteps.buildQueryParamMapForPaging;
@@ -12,20 +11,18 @@ import static travel.snapshot.dp.qa.junit.utils.DpEndpoints.READ_WRITE_ENDPOINTS
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import qa.tools.ikeeper.annotation.Jira;
 import travel.snapshot.dp.qa.junit.tests.common.CommonTest;
-import travel.snapshot.dp.qa.junit.utils.QueryParams;
 
-import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.Map;
-import java.util.stream.Stream;
 
 
 class AllEndpointsParameterizedTests extends CommonTest {
 
     private static final String EXAMPLES = "/csv/all_endpoints/";
-    private static final String EMPTY_ARGS = "";
+    private static final String EMPTY_PARAM_VALUE = "";
 
 
     @ParameterizedTest
@@ -48,40 +45,13 @@ class AllEndpointsParameterizedTests extends CommonTest {
     }
 
     @ParameterizedTest
-    @MethodSource("getQueryParamsMethods")
-    void emptyGetParamsAreIgnored(Method method) {
-        READ_WRITE_ENDPOINTS.forEach(endpoint -> getEntities(endpoint, getParamsFromMethod(method))
+    @ValueSource(strings = {"limit", "cursor", "filter", "sort", "sortDesc"})
+    void emptyGetParamsAreIgnored(String param) {
+        Map<String, String> queryParams = Collections.singletonMap(param, EMPTY_PARAM_VALUE);
+        READ_WRITE_ENDPOINTS.forEach(endpoint -> getEntities(endpoint, queryParams)
                 .then()
                 .statusCode(SC_OK));
 
-    }
-
-    /**
-     * Gets all QueryParams (filter, cursor, sort...) as QueryParamsBuilder methods. Can be used for invoking the
-     * methods on the builder object (separately) to test all the queryParams (separately)
-     */
-    private static Stream<Method> getQueryParamsMethods() {
-        try {
-            final Method buildMethod = QueryParams.QueryParamsBuilder.class.getDeclaredMethod("build", null);
-            final Method additionalParamsMethod = QueryParams.QueryParamsBuilder.class.getDeclaredMethod("additionalParams", Map.class);
-
-            return stream(QueryParams.QueryParamsBuilder.class.getDeclaredMethods())
-                    .filter(method -> !method.equals(buildMethod))
-                    .filter(method -> !method.equals(additionalParamsMethod));
-        } catch (Exception e) {
-            throw new RuntimeException(e.getCause());
-        }
-    }
-
-    private Map<String, String> getParamsFromMethod(Method method){
-        try {
-            QueryParams.QueryParamsBuilder queryParamsBuilder = QueryParams.builder();
-            method.invoke(queryParamsBuilder, EMPTY_ARGS);
-
-            return queryParamsBuilder.build();
-        } catch (Exception e) {
-            throw new RuntimeException("Exception thrown when invoking method " + method, e.getCause());
-        }
     }
 
 }
