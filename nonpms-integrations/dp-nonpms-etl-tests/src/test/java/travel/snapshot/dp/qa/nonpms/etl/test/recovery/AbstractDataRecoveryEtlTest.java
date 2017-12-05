4213d6f411b80a3ce74ab04e153e682e0853f47c
Adam Hasik <adam.hasik@snapshot.travel>
2017-12-05 11:15:23 +0100

    DPNP-182: Added ETL data recovery test for past integration

--- nonpms-integrations/dp-nonpms-etl-tests/src/test/java/travel/snapshot/dp/qa/nonpms/etl/test/recovery/AbstractDataRecoveryEtlTest.java
+++ nonpms-integrations/dp-nonpms-etl-tests/src/test/java/travel/snapshot/dp/qa/nonpms/etl/test/recovery/AbstractDataRecoveryEtlTest.java
@@ -3,9 +3,9 @@ package travel.snapshot.dp.qa.nonpms.etl.test.recovery;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static travel.snapshot.dp.qa.nonpms.etl.util.DateUtils.toDateId;
-import static travel.snapshot.dp.qa.nonpms.etl.util.Helpers.createSchedulerMessageJson;
 import static travel.snapshot.dp.qa.nonpms.etl.util.Helpers.extractLocalDate;
 import static travel.snapshot.dp.qa.nonpms.etl.util.Helpers.extractTimestamp;
+import static travel.snapshot.dp.qa.nonpms.etl.util.JsonConverter.convertToJson;
 
 import lombok.Getter;
 import org.junit.Before;
@@ -16,15 +16,20 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import travel.snapshot.dp.qa.nonpms.etl.messages.SchedulerMessage;
+import travel.snapshot.dp.qa.nonpms.etl.messages.SchedulerMessage.SchedulerMessageBuilder;
 import travel.snapshot.dp.qa.nonpms.etl.test.AbstractEtlTest;
 import travel.snapshot.dp.qa.nonpms.etl.test.dal.IntegrationDwhDao;
 
 import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @ImportAutoConfiguration({
         DataSourceTransactionManagerAutoConfiguration.class,
@@ -35,7 +40,7 @@ import java.util.Map;
 @MapperScan("travel.snapshot.dp.qa.nonpms.etl.test.dal")
 public abstract class AbstractDataRecoveryEtlTest extends AbstractEtlTest {
 
-    static final String IGNORED_FIRE_TIME = "2017-01-01T00:00:00Z";
+    static final Instant IGNORED_FIRE_TIME = Instant.parse("2017-01-01T00:00:00Z");
 
     protected abstract IntegrationDwhDao getIntegrationDwhDao();
     protected abstract String getTimezone();
@@ -57,34 +62,71 @@ public abstract class AbstractDataRecoveryEtlTest extends AbstractEtlTest {
     }
 
     @Test(timeout = 60000)
-    public void testEtlForDataRecovery() throws Exception {
+    public void testEtlDataRecovery() throws Exception {
         LocalDateTime start = getCurrentTimestamp();
 
         createLastDataGap(DAY1, DAY5);
 
-        jms.send(getStartQueue(), createSchedulerMessageJson(createFireTimeForMidnight(DAY5, getTimezone())));
+        start(id -> SchedulerMessage.builder().fireTime(createFireTimeForMidnight(DAY5, getTimezone())));
 
         checkReloadedDays(asList(DAY2, DAY3, DAY4, DAY5), start);
     }
 
     @Test(timeout = 60000)
-    public void testEtlForDataRecoveryForCustomIntegration() throws Exception {
+    public void testEtlDataRecoveryForCustomIntegration() throws Exception {
         LocalDateTime start = getCurrentTimestamp();
 
         createLastDataGap(DAY1, DAY5);
 
-        for (String propertyId : getAffectedProperties()) {
-            jms.send(getStartQueue(), createSchedulerMessageJson(IGNORED_FIRE_TIME, propertyId, DAY5.toString()));
-        }
+        start(id -> SchedulerMessage.builder().fireTime(IGNORED_FIRE_TIME).propertyId(id).integrationDate(DAY5));
 
         checkReloadedDays(asList(DAY2, DAY3, DAY4, DAY5), start);
     }
 
+    @Test(timeout = 60000)
+    public void testEtlDataRecoveryForPastCustomIntegration() throws Exception {
+        LocalDateTime start = getCurrentTimestamp();
+
+        createLastDataGap(DAY1, DAY5);
+
+        start(id -> SchedulerMessage.builder().fireTime(IGNORED_FIRE_TIME).propertyId(id).integrationDate(DAY2).isCurrentDay(false));
+
+        checkReloadedDays(asList(DAY2), start);
+    }
+
+    @Test(timeout = 60000)
+    public void testEtlDataRecoveryForPastCustomIntegrationWithOverrideData() throws Exception {
+        LocalDateTime start = getCurrentTimestamp();
+
+        createLastDataGap(DAY1, DAY5);
+        insertData(DAY2);
+
+        for (String propertyId : getAffectedProperties()) {
+            jms.send(getStartQueue(), convertToJson(SchedulerMessage.builder()
+                    .fireTime(IGNORED_FIRE_TIME).propertyId(propertyId).integrationDate(DAY2)
+                    .isCurrentDay(false).overrideData(true).build()));
+        }
+
+        checkReloadedDays(asList(DAY2), start);
+    }
+
     private void createLastDataGap(LocalDate since, LocalDate until) {
-        getAffectedProperties().forEach(propertyId -> {
-            getIntegrationDwhDao().deleteData(propertyId, toDateId(since), toDateId(until));
-            getIntegrationDwhDao().insertData(propertyId, toDateId(since));
-        });
+        deleteData(since, until);
+        insertData(since);
+    }
+
+    private void deleteData(LocalDate since, LocalDate until) {
+        runForAffectedProperties(id -> getIntegrationDwhDao().deleteData(id, toDateId(since), toDateId(until)));
+    }
+
+    private void insertData(LocalDate date) {
+        runForAffectedProperties(id -> getIntegrationDwhDao().insertData(id, toDateId(date)));
+    }
+
+    private void start(Function<String, SchedulerMessageBuilder> schedulerMessageFactory) throws Exception {
+        for (String propertyId : getAffectedProperties()) {
+            jms.send(getStartQueue(), convertToJson(schedulerMessageFactory.apply(propertyId).build()));
+        }
     }
 
     private void checkReloadedDays(List<LocalDate> days, LocalDateTime timestamp) throws Exception {
@@ -96,6 +138,7 @@ public abstract class AbstractDataRecoveryEtlTest extends AbstractEtlTest {
             checkReloadedData(day, timestamp);
         }
     }
+
     private void checkReloadedData(LocalDate day, LocalDateTime timestamp) {
         getAffectedProperties().forEach(propertyId -> {
             Map<String, Object> data = getIntegrationDwhDao().getData(propertyId, toDateId(affectedDate));
@@ -104,8 +147,12 @@ public abstract class AbstractDataRecoveryEtlTest extends AbstractEtlTest {
         });
     }
 
-    private static String createFireTimeForMidnight(LocalDate date, String timezone) {
-        return date.plusDays(1).atStartOfDay(ZoneId.of(timezone)).toInstant().toString();
+    private void runForAffectedProperties(Consumer<String> call) {
+        getAffectedProperties().forEach(propertyId -> call.accept(propertyId));
+    }
+
+    private static Instant createFireTimeForMidnight(LocalDate date, String timezone) {
+        return date.plusDays(1).atStartOfDay(ZoneId.of(timezone)).toInstant();
     }
 
     private static LocalDateTime getCurrentTimestamp() {
